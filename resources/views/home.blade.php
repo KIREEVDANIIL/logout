e7616485eda544bf56c4f87a4c608886b6cd1c85
KIREEVDANIIL <146159768+KIREEVDANIIL@users.noreply.github.com>
2025-10-22 16:43:33 +0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,12 @@
 <x-layout>
     <div class="bg-white p-6 rounded shadow">
         <h1 class="text-2xl font-bold mb-4">Главная страница</h1>
-        
+        @auth
+            <div class=" p-4 rounded mb-4">
+                <h2 class="text-xl mb-2">{{ Auth::user()->fio }}!</h2>
+            </div>
+        @endauth
+    
+    </div>
     </div>
 </x-layout>
