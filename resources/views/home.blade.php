6261813412475f6fc6e71ff188bf249790af953e
KIREEVDANIIL <146159768+KIREEVDANIIL@users.noreply.github.com>
2025-10-23 00:21:52 +0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,13 @@
 <x-layout>
-    <div class="bg-white p-6 rounded shadow">
-        <h1 class="text-2xl font-bold mb-4">Главная страница</h1>
+    <div class="bg-white p-6 rounded ">
+        <h1 class="text-2xl font-bold mb-4 ml-6">Главная страница</h1>
+        
         @auth
-            <div class=" p-4 rounded mb-4">
-                <h2 class="text-xl mb-2">{{ Auth::user()->fio }}!</h2>
+            <div class="p-4 rounded mb-4 ml-2">
+                <h2 class="text-xl mb-2 font">Здравствуйте, {{ Auth::user()->fio }}, на этом сайте выкладываются различные посты</h2>
             </div>
         @endauth
-    
-    </div>
+        
+
     </div>
 </x-layout>
